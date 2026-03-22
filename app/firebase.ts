66afc3a4aff4fdb3a3b11f7bb0c99b5b833a7a5b
siddarth <siddarth.kadambari@gmail.com>
2026-03-22 14:53:05 +0530

    test commit

--- app/firebase.ts
+++ app/firebase.ts
@@ -7,15 +7,16 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCjruu4jjmbIB1JOO2zBkNo6i89ok37aMc",
-  authDomain: "oneiitbbs.firebaseapp.com",
-  projectId: "oneiitbbs",
-  storageBucket: "oneiitbbs.firebasestorage.app",
-  messagingSenderId: "208282390520",
-  appId: "1:208282390520:web:2ee28e7045c001a2d2d00d",
-  measurementId: "G-DTDHB683HV"
+    apiKey: "AIzaSyCjruu4jjmbIB1JOO2zBkNo6i89ok37aMc",
+    authDomain: "oneiitbbs.firebaseapp.com",
+    projectId: "oneiitbbs",
+    storageBucket: "oneiitbbs.firebasestorage.app",
+    messagingSenderId: "208282390520",
+    appId: "1:208282390520:web:2ee28e7045c001a2d2d00d",
+    measurementId: "G-DTDHB683HV"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const db = getFirestore(app);
+export default db;
